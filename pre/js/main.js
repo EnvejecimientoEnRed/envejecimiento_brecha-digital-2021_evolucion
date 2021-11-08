61c3112d72139d0659c2468efdce309b4274a853
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-11-08 13:51:21 +0100

    Desarrollo primario del gráfico

--- pre/js/main.js
+++ pre/js/main.js
@@ -8,381 +8,146 @@ import * as d3 from 'd3';
 import '../css/main.scss';
 
 ///// VISUALIZACIÓN DEL GRÁFICO //////
-let dataSources = [
-    'https://raw.githubusercontent.com/EnvejecimientoEnRed/longevidad_edv-saludable_barras/main/data/ine_data.csv', 
-    'https://raw.githubusercontent.com/EnvejecimientoEnRed/longevidad_edv-saludable_barras/main/data/echo_data.csv', 
-    'https://raw.githubusercontent.com/EnvejecimientoEnRed/longevidad_edv-saludable_barras/main/data/renteria_data.csv',
-    'https://raw.githubusercontent.com/EnvejecimientoEnRed/longevidad_edv-saludable_barras/main/data/renteria-ced_data.csv'
-];
+let dataSource = 'https://raw.githubusercontent.com/CarlosMunozDiazCSIC/envejecimiento_brecha-digital-2021_evolucion/main/data/evolucion_edad_tic.csv';
 let tooltip = d3.select('#tooltip');
 
-let ineData = [], echoData = [], echoDfleData = [], echoChrdfleData = [], renteriaData = [], renteriaCedData = [], currentData = [];
-let subgrupoHombres = ['hombres_saludable','hombres_no_saludable'];
-let subgrupoMujeres = ['mujeres_saludable','mujeres_no_saludable'];
-let chartMenBlock = d3.select('#chartMen'), chartMen, xChartMen, xAxisChartMen, yChartMen, yAxisChartMen;
-let chartWomenBlock = d3.select('#chartWomen'), chartWomen, xChartWomen, xAxisChartWomen, yChartWomen, yAxisChartWomen;
-let margin = {top: 4, right: 5, bottom: 17.5, left: 22.5}, width, height;
-let colors = ['#78bb6e','#6f0910'];
+let innerData = [], nestedData = [], chartBlock = d3.select('#chart'), chart, x_c, x_cAxis, y_c, y_cAxis, line, paths;
+let margin = {top: 5, right: 10, bottom: 17.5, left: 35}, width, height;
+let colors = ['#78bb6e', '#6f0910', '#cecece'];
 
-initData();
-
-function initData() {
-    let q = d3.queue();
-    let csv = d3.dsvFormat(';');
-
-    q.defer(d3.text, dataSources[0]);
-    q.defer(d3.text, dataSources[1]);
-    q.defer(d3.text, dataSources[2]);
-    q.defer(d3.text, dataSources[3]);
-
-    q.await(function(err, ine, echo, renteria, renteria_ced) {
-        if (err) throw err;
-
-        //INE
-        ineData = csv.parse(ine);
-        ineData = ineData.map(function(d) {
-            return {
-                'anio' : d.anio,
-                'hombres_saludable' : +d.hombres_saludable.replace(',','.'),
-                'hombres_no_saludable' : +d.hombres_total.replace(',','.') - +d.hombres_saludable.replace(',','.'),
-                'mujeres_saludable' : +d.mujeres_saludable.replace(',','.'),
-                'mujeres_no_saludable' : +d.mujeres_total.replace(',','.') - +d.mujeres_saludable.replace(',','.')
-            }
-        });
-        
-        //PAPER RENTERÍA
-        renteriaData = csv.parse(renteria);
-        renteriaData = renteriaData.map(function(d) {
-            return {
-                'anio' : d.anio,
-                'hombres_saludable' : +d.hombres_sin.replace(',','.'),
-                'hombres_no_saludable' : +d.hombres_con.replace(',','.'),
-                'mujeres_saludable' : +d.mujeres_sin.replace(',','.'),
-                'mujeres_no_saludable' : +d.mujeres_con.replace(',','.')
-            }
-        });
-
-        //CED RENTERÍA
-        renteriaCedData = csv.parse(renteria_ced);
-        renteriaCedData = renteriaCedData.map(function(d) {
-            return {
-                'anio' : d.anio,
-                'hombres_saludable' : +d.hombres_sin.replace(',','.'),
-                'hombres_no_saludable' : +d.hombres_con.replace(',','.'),
-                'mujeres_saludable' : +d.mujeres_sin.replace(',','.'),
-                'mujeres_no_saludable' : +d.mujeres_con.replace(',','.')
-            }
-        });
-
-        //Distinción para ECHO
-        echoData = csv.parse(echo);
-        echoDfleData[0] = { 
-            'anio' : echoData[0]['anio'], 
-            'hombres_no_saludable' :  +echoData[0]['hombres_con_dfle'].replace(',','.'), 
-            'hombres_saludable' : +echoData[0]['hombres_sin_dfle'].replace(',','.'), 
-            'mujeres_no_saludable' :  +echoData[0]['mujeres_con_dfle'].replace(',','.'), 
-            'mujeres_saludable' : +echoData[0]['mujeres_sin_dfle'].replace(',','.')
-        }
-        echoDfleData[1] = { 
-            'anio' : echoData[1]['anio'], 
-            'hombres_no_saludable' :  +echoData[1]['hombres_con_dfle'].replace(',','.'), 
-            'hombres_saludable' : +echoData[1]['hombres_sin_dfle'].replace(',','.'), 
-            'mujeres_no_saludable' :  +echoData[1]['mujeres_con_dfle'].replace(',','.'), 
-            'mujeres_saludable' : +echoData[1]['mujeres_sin_dfle'].replace(',','.')
-        }
-
-        echoChrdfleData[0] = { 
-            'anio' : echoData[0]['anio'], 
-            'hombres_no_saludable' :  +echoData[0]['hombres_con_chrdfle'].replace(',','.'), 
-            'hombres_saludable' : +echoData[0]['hombres_sin_chrdfle'].replace(',','.'), 
-            'mujeres_no_saludable' :  +echoData[0]['mujeres_con_chrdfle'].replace(',','.'), 
-            'mujeres_saludable' : +echoData[0]['mujeres_sin_chrdfle'].replace(',','.')
-        }
-        echoChrdfleData[1] = { 
-            'anio' : echoData[1]['anio'], 
-            'hombres_no_saludable' :  +echoData[1]['hombres_con_chrdfle'].replace(',','.'), 
-            'hombres_saludable' : +echoData[1]['hombres_sin_chrdfle'].replace(',','.'), 
-            'mujeres_no_saludable' :  +echoData[1]['mujeres_con_chrdfle'].replace(',','.'), 
-            'mujeres_saludable' : +echoData[1]['mujeres_sin_chrdfle'].replace(',','.')
-        }
-
-        //De primeras, mostramos los datos del INE
-        initChart();
-
-        setTimeout(() => {
-            setChartCanvas();
-        }, 5000);
-    });    
-}
+initChart();
 
 function initChart() {
-    width = parseInt(chartMenBlock.style('width')) - margin.left - margin.right - 5;
-    height = parseInt(chartMenBlock.style('height')) - margin.top - margin.bottom;
+    let csv = d3.dsvFormat(';');
 
-    menChart(ineData);
-    womenChart(ineData);   
+    d3.text(dataSource, function(err, data) {
+        if(err) throw err;
+        data = csv.parse(data);
+
+        innerData = data.map(function(d) {
+            return {
+                anio: d.anio,
+                '16_24': +d['16_24'].replace(',','.'),
+                '25_34': +d['25_34'].replace(',','.'),
+                '35_44': +d['35_44'].replace(',','.'),
+                '45_54': +d['45_54'].replace(',','.'),
+                '55_64': +d['55_64'].replace(',','.'),
+                '65_74': +d['65_74'].replace(',','.')
+            }
+        });
+        innerData = innerData.reverse();
+
+        let keys = data.columns.slice(2);
+
+        nestedData = keys.map(function(item) {
+            let aux = [];
+            innerData.map(function(d) {
+                aux.push({'anio':d.anio, 'valor': d[item]});
+            });
+            return {'key': item, 'data': aux};            
+        });
+
+        //Desarrollo de la visualización
+        width = parseInt(chartBlock.style('width')) - margin.left - margin.right,
+        height = parseInt(chartBlock.style('height')) - margin.top - margin.bottom;
+
+        chart = chartBlock
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+        x_c = d3.scaleBand()
+            .domain(nestedData[0].data.map(function(d) { return d.anio; }))
+            .range([0, width]);
+
+        chart.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x_c));
+
+        // Add Y axis
+        y_c = d3.scaleLinear()
+            .domain([0, 100])
+            .range([height, 0]);
+
+        chart.append("g")
+            .call(d3.axisLeft(y_c));
+
+        //Línea
+        line = d3.line()
+            .x(function(d) { return x_c(d.anio) + x_c.bandwidth() / 2; })
+            .y(function(d) { return y_c(+d.valor); })
+            .curve(d3.curveNatural);
+
+        paths = chart.selectAll(".line")
+            .data(nestedData)
+            .enter()
+            .append("path")
+            .attr('class', 'line')
+            .attr("fill", "none")
+            .attr("stroke", function(d){
+                if (d.key == '65_74') {
+                    return colors[0];
+                } else {
+                    return colors[2];
+                }
+            })
+            .attr("stroke-width", 2)
+            .attr('d', function(d) {
+                return line(d.data);
+            });
+
+        paths.attr("stroke-dasharray", 768 + " " + 768)
+            .attr("stroke-dashoffset", 768)
+            .transition()
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+            .duration(3000);
+    }); 
 }
 
 function updateChart(tipo) {
-    switch(tipo) {
-        case 'ine':
-            currentData = ineData.slice();
-            break;
-        case 'echo-dfle':
-            currentData = echoDfleData.slice();
-            break;
-        case 'echo-chrdfle':
-            currentData = echoChrdfleData.slice();
-            break;
-        case 'renteria-plos':
-            currentData = renteriaData.slice();
-            break;
-        case 'renteria-ced':
-            currentData = renteriaCedData.slice();
-            break;
-        default:
-            currentData = ineData.slice();
-            break;
-    }
+    //Cambiamos el color a las líneas
+    console.log(tipo);
 
-    //Borrado previo
-    chartMenBlock.selectAll('*').remove();
-    chartWomenBlock.selectAll('*').remove();
+    //Cambiamos la variable
 
-    //Disposición de nueva información
-    menChart(currentData);
-    womenChart(currentData);
 
+    //Ponemos los datos de la brecha
+    
+    
+    //Hacemos nueva fotografía de la visualización
+    setTimeout(() => {
+        setChartCanvas();
+    }, 5000);
+}
+
+function animateChart() {
+    //Que evolucionen de nuevo con los mismos colores que tenían
+    paths = chart.selectAll(".line")
+        .attr("stroke-width", 2)
+        .attr('d', function(d) {
+            return line(d.data);
+        });
+
+    paths.attr("stroke-dasharray", 768 + " " + 768)
+        .attr("stroke-dashoffset", 768)
+        .transition()
+        .ease(d3.easeLinear)
+        .attr("stroke-dashoffset", 0)
+        .duration(3000);
+
+    //Hacemos nueva fotografía de la visualización
     setTimeout(() => {
         setChartCanvas();
     }, 5000);
 }
 
 document.getElementById('replay').addEventListener('click', function() {
-    updateChart(currentSelected);
+    animateChart();
 });
-
-//Helpers en visualización
-function menChart(data) {
-    ////// HOMBRES //////
-    chartMen = chartMenBlock
-        .append('svg')
-        .lower()
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    //Eje X
-    xChartMen = d3.scaleBand()
-        .domain(data.map(function(d) { return d.anio; }))
-        .range([0, width]);
-
-    xAxisChartMen = function(g) {
-        g.call(d3.axisBottom(xChartMen).tickValues(xChartMen.domain().filter(function(d,i){
-            if(currentSelected == 'ine') {
-                return !(i%3);
-            } else {
-                return !(i%1);
-            }            
-        })))
-        //g.call(function(g){g.select('.domain').remove()});
-    }
-
-    chartMen.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxisChartMen);
-
-    //Eje Y
-    yChartMen = d3.scaleLinear()
-        .domain([0,25])
-        .range([height, 0]);
-
-    yAxisChartMen = function(g) {
-        g.call(d3.axisLeft(yChartMen).ticks(5));
-    }
-
-    chartMen.append("g")
-        .call(yAxisChartMen);
-
-    //SUBBLOQUES ANUALES
-    let stackedMenData = d3.stack()
-        .keys(subgrupoHombres)
-        (data);
-
-    chartMen.append("g")
-        .selectAll("g")
-        // Enter in the stack data = loop key per key = group per group
-        .data(stackedMenData)
-        .enter()
-        .append("g")
-        .attr("fill", function(d) {
-            if(d.index == 0) {
-                return colors[0];
-            } else {
-                return colors[1];
-            }
-        })
-        .selectAll("rect")
-        // enter a second time = loop subgroup per subgroup to add all rectangles
-        .data(function(d) { return d; })
-        .enter()
-        .append("rect")
-        .attr('class', function(d) { return 'bar hombres-' + d.data.anio; })
-        .attr("x", function(d) { return xChartMen(d.data.anio) + xChartMen.bandwidth() / 4; })
-        .attr("y", function(d) { return yChartMen(0); })
-        .attr("width", xChartMen.bandwidth() / 2)
-        .on('mouseover', function(d, i, e) {
-            let total = d.data.hombres_saludable + d.data.hombres_no_saludable;
-
-            let html = `<p class="chart__tooltip--title">${d.data.anio}</p>
-                <p class="chart__tooltip--text">EdV total: ${total.toFixed(1).replace('.',',')} años</p>
-                <p class="chart__tooltip--text">EdV en buena salud: ${d.data.hombres_saludable.toFixed(1).replace('.',',')} años</p>
-                <p class="chart__tooltip--text">EdV con discapacidad y/o enfermedades: ${d.data.hombres_no_saludable.toFixed(1).replace('.',',')} años</p>`;
-
-            tooltip.html(html);
-
-            //Posibilidad visualización línea diferente
-            let bars = d3.selectAll('.bar');
-            let css = e[i].getAttribute('class').split(' ')[1];
-
-            bars.each(function() {
-                this.style.opacity = '0.4';
-                if(this.getAttribute('class').indexOf(`${css}`) != -1) {
-                    this.style.opacity = '1';
-                }
-            });
-
-            //Tooltip
-            positionTooltip(window.event, tooltip);
-            getInTooltip(tooltip);
-        })
-        .on('mouseout', function(d) {
-            //Quitamos los estilos de la línea
-            let bars = d3.selectAll('.bar');
-            bars.each(function() {
-                this.style.opacity = '1';
-            });
-
-            //Quitamos el tooltip
-            getOutTooltip(tooltip); 
-        })
-        .transition()
-        .duration(1500)
-        .attr("y", function(d) { return yChartMen(d[1]); })
-        .attr("height", function(d) { return yChartMen(d[0]) - yChartMen(d[1]); });
-}
-
-function womenChart(data) {
-    ////// MUJERES //////
-    chartWomen = chartWomenBlock
-        .append('svg')
-        .lower()
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    //Eje X
-    xChartWomen = d3.scaleBand()
-        .domain(data.map(function(d) { return d.anio; }))
-        .range([0, width]);
-
-    xAxisChartWomen = function(g) {
-        g.call(d3.axisBottom(xChartWomen).tickValues(xChartWomen.domain().filter(function(d,i){
-            if(currentSelected == 'ine') {
-                return !(i%3);
-            } else {
-                return !(i%1);
-            }
-        })))
-        //g.call(function(g){g.select('.domain').remove()});
-    }
-
-    chartWomen.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxisChartWomen);
-
-    //Eje Y
-    yChartWomen = d3.scaleLinear()
-        .domain([0,25])
-        .range([height, 0]);
-
-    yAxisChartWomen = function(g) {
-        g.call(d3.axisLeft(yChartWomen).ticks(5));
-    }
-
-    chartWomen.append("g")
-        .call(yAxisChartWomen);
-
-    //Datos
-    let stackedWomenData = d3.stack()
-        .keys(subgrupoMujeres)
-        (data);
-
-    chartWomen.append("g")
-        .selectAll("g")
-        // Enter in the stack data = loop key per key = group per group
-        .data(stackedWomenData)
-        .enter()
-        .append("g")
-        .attr("fill", function(d) {
-            if(d.index == 0) {
-                return colors[0];
-            } else {
-                return colors[1];
-            }
-        })
-        .selectAll("rect")
-        // enter a second time = loop subgroup per subgroup to add all rectangles
-        .data(function(d) { return d; })
-        .enter()
-        .append("rect")
-        .attr('class', function(d) { return 'bar mujeres-' + d.data.anio; })
-        .attr("x", function(d) { return xChartWomen(d.data.anio) + xChartWomen.bandwidth() / 4; })
-        .attr("y", function(d) { return yChartWomen(0); })
-        .attr("width", xChartWomen.bandwidth() / 2)
-        .on('mouseover', function(d, i, e) {
-            //Texto
-            let total = d.data.mujeres_saludable + d.data.mujeres_no_saludable;
-
-            let html = `<p class="chart__tooltip--title">${d.data.anio}</p>
-                <p class="chart__tooltip--text">EdV total: ${total.toFixed(1).replace('.',',')} años</p>
-                <p class="chart__tooltip--text">EdV en buena salud: ${d.data.mujeres_saludable.toFixed(1).replace('.',',')} años</p>
-                <p class="chart__tooltip--text">EdV con discapacidad y/o enfermedades: ${d.data.mujeres_no_saludable.toFixed(1).replace('.',',')} años</p>`;
-
-            tooltip.html(html);
-
-            //Posibilidad visualización línea diferente
-            let bars = d3.selectAll('.bar');
-            let css = e[i].getAttribute('class').split(' ')[1];
-
-            bars.each(function() {
-                this.style.opacity = '0.4';
-                if(this.getAttribute('class').indexOf(`${css}`) != -1) {
-                    this.style.opacity = '1';
-                }
-            });
-
-            //Tooltip
-            positionTooltip(window.event, tooltip);
-            getInTooltip(tooltip);
-        })
-        .on('mouseout', function(d) {
-            //Quitamos los estilos de la línea
-            let bars = d3.selectAll('.bar');
-            bars.each(function() {
-                this.style.opacity = '1';
-            });
-
-            //Quitamos el tooltip
-            getOutTooltip(tooltip); 
-        })
-        .transition()
-        .duration(1500)
-        .attr("y", function(d) { return yChartWomen(d[1]); })
-        .attr("height", function(d) { return yChartWomen(d[0]) - yChartWomen(d[1]); });
-}
 
 ///// REDES SOCIALES /////
 setRRSSLinks();
@@ -436,7 +201,7 @@ function setChartCanvasImage() {
     // Create a link
     var aDownloadLink = document.createElement('a');
     // Add the name of the file to the link
-    aDownloadLink.download = 'longevidad_edv_saludable.png';
+    aDownloadLink.download = 'envejecimiento_brecha-digital-2021_evolucion.png';
     // Attach the data to the link
     aDownloadLink.href = image;
     // Get the code to click the download link
@@ -478,7 +243,7 @@ function displayContainer(elem) {
 
 ///// USO DE SELECTORES //////
 let x, i, j, l, ll, selElmnt, a, b, c;
-let currentSelected = 'ine';
+let currentSelected = 'ninguno';
 /* Look for any elements with the class "custom-select": */
 x = document.getElementsByClassName("custom-select");
 l = x.length;
@@ -497,7 +262,7 @@ for (i = 0; i < l; i++) {
     /* For each option in the original select element,
     create a new DIV that will act as an option item: */
     c = document.createElement("DIV");
-    let valores = selElmnt.options[j].value.split("_");
+    let valores = selElmnt.options[j].value.split("-");
     c.setAttribute('data-value', valores[0]);
     c.setAttribute('data-type', valores[1]);
     c.innerHTML = selElmnt.options[j].innerHTML;
